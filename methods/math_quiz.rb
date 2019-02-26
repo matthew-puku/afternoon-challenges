8025ac8de549dffe02573d982355235730e9a22b
Matthew Puku <matthew.puku@gmail.com>
2019-02-26 16:17:56 +1000

    refactored and commented math quiz.

--- methods/math_quiz.rb
+++ methods/math_quiz.rb
@@ -1,41 +1,34 @@
-# When the user launches the game, she is being greeted.
-# Next, the program poses a simple maths question (e.g. “What’s 26 * 4?”). The two numbers are chosen randomly from the range 1 to 100. The operation is also chosen randomly. It’s either addition, subtraction or multiplication.
-# The user can then enter their answer.
-# The game checks the answer and responds with either “Correct!” or “Wrong!”.
-# The game loops until the user enters “quit”.
-# At first, the task to implement something like that may seem daunting. As always, try to think about the smallest thing you can program which will get you closer to your goal. Do that, check that it’s working and then move on with the next tiny step.
-
 looping = true
 prng = Random.new
 
-def generate_problem(operation) # Generates a math problem and its answer, stored in a hash. Passing a 0 generates an addition problem, 1 subtraction, and 2 multiplication.
+def generate_problem(operation) # Generates a math problem and its answer, stored in a hash. Passing a 0 generates an addition problem, 1 subtraction, and 2 multiplication. You can also pass a string.
     prng = Random.new
+    factor1 = prng.rand(1..100)
+    factor2 = prng.rand(1..100)
+    problem = {}
     case operation
-    when 0
-        factor1 = prng.rand(1..100)
-        factor2 = prng.rand(1..100)
-        problem = "What's #{factor1} + #{factor2}?"
+    when 0, "addition"
+        prompt = "What's #{factor1} + #{factor2}?"
         answer = factor1 + factor2
-        return {:problem => problem, :answer => answer}
-    when 1
-        factor1 = prng.rand(1..100)
-        factor2 = prng.rand(1..100)
-        problem = "What's #{factor1} - #{factor2}?"
+        problem = {:prompt => prompt, :answer => answer}
+    when 1, "subtraction"
+        prompt = "What's #{factor1} - #{factor2}?"
         answer = factor1 - factor2
-        return {:problem => problem, :answer => answer}
-    when 2
-        factor1 = prng.rand(1..100)
-        factor2 = prng.rand(1..100)
-        problem = "What's #{factor1} * #{factor2}?"
+        problem = {:prompt => prompt, :answer => answer}
+    when 2, "multiplication"
+        prompt = "What's #{factor1} * #{factor2}?"
         answer = factor1 * factor2
-        return {:problem => problem, :answer => answer}
+        problem = {:prompt => prompt, :answer => answer}
+    else # bad inputs raise exception with informative message
+        raise(ArgumentError, "Expected 0, 1, 2, 'addition', 'subtraction', or 'multiplication'. Actual: #{operation}")
     end
+    return problem
 end
 
 puts "Welcome to math_quiz.rb! Answer problems or type quit to exit."
 while looping
     question = generate_problem(prng.rand(3))
-    puts question[:problem]
+    puts question[:prompt]
     input = gets.chomp.downcase
     case input
     when "exit", "quit", "e", "q", "ZZ", "end", "done", "stop"
@@ -48,3 +41,4 @@ while looping
         end
     end
 end
+puts "Ciao!"
